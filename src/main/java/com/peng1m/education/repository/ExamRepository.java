c98e3920c4098dbb4141509e2501c22477d23f6e
Peng-YM <1048217874pengym@gmail.com>
2018-05-26 21:45:46 +0800

    Add security detail

--- src/main/java/com/peng1m/education/repository/ExamRepository.java
+++ src/main/java/com/peng1m/education/repository/ExamRepository.java
@@ -2,11 +2,33 @@ package com.peng1m.education.repository;
 
 import com.peng1m.education.model.Exam;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.Optional;
 
 @RepositoryRestResource(
         collectionResourceRel = "exams",
         path = "exams")
+@PreAuthorize("hasRole('USER')")
 public interface ExamRepository extends PagingAndSortingRepository<Exam, Long> {
+    /**
+     * Only allows admin to edit exam info
+     * @param s exam
+     * @param <S> Exam
+     * @return Exam object
+     */
+    @PreAuthorize("hasRole('ADMIN')")
+    @Override
+    <S extends Exam> S save(S s);
 
+    /**
+     * Only allow admin to delete exam
+     * @param id
+     */
+    @PreAuthorize("hasRole('ADMIN')")
+    @Override
+    void deleteById(Long id);
 }
--- src/main/java/com/peng1m/education/repository/MarkRepository.java
+++ src/main/java/com/peng1m/education/repository/MarkRepository.java
@@ -3,11 +3,23 @@ package com.peng1m.education.repository;
 import com.peng1m.education.model.Mark;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.Optional;
 
 @RepositoryRestResource(
         collectionResourceRel = "marks",
         path = "marks"
 )
+@PreAuthorize("hasRole('ADMIN')")
 public interface MarkRepository extends PagingAndSortingRepository<Mark, Long> {
-
+    /**
+     * Only allow admin and student himself to check the score.
+     * @param mark_id mark_id
+     * @return Mark
+     */
+    @PostAuthorize("hasRole('ADMIN') or returnObject.get().student.credential.username == principal.username")
+    @Override
+    Optional<Mark> findById(Long mark_id);
 }
