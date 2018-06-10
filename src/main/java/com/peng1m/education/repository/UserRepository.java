869ebf8d5d5d64a012acbc29177b8770ecf7d069
Peng-YM <1048217874pengym@gmail.com>
2018-06-10 10:54:14 +0800

    Add paging and sorting to user API

--- src/main/java/com/peng1m/education/repository/UserRepository.java
+++ src/main/java/com/peng1m/education/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.peng1m.education.model.User;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PostAuthorize;
@@ -18,7 +19,7 @@ import java.util.Optional;
         path = "users"
 )
 @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     /**
      * Find user by email
      * example usage: GET /users/search/findByEmail?email=someone@somewhere.com
